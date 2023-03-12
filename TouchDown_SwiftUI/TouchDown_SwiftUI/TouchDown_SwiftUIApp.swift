257f302948bd9a66c0af1dbbf44b24ebfaed9c07
Denys Melnyk <georgiemuler@gmail.com>
2023-03-12 13:08:57 +0200

    Initital commit

---
+++ TouchDown_SwiftUI/TouchDown_SwiftUI/TouchDown_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDown_SwiftUIApp.swift
+//  TouchDown_SwiftUI
+//
+//  Created by Georgie Muler on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchDown_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
